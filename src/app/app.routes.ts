aa6e3eb6a486527cbb0bbc030a03da972a7efc65
SamuelCano03 <U202116508@upc.edu.pe>
2025-05-26 07:27:15 -0500

    feature login completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './landing/home/home.component';
-
+import { LoginComponent } from './auth/login/login.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
 ];
