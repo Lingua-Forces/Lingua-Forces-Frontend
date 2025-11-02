d5f7065e8a6007968673b4842e7e3b155238a3e2
SamuelCano03 <U202116508@upc.edu.pe>
2025-11-02 18:33:48 -0500

    fixes on responsive design

--- src/app/landing/home/home.component.ts
+++ src/app/landing/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router'; 
+import { AuthHeaderComponent } from '../../shared/auth-header/auth-header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, AuthHeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
